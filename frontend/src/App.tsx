b2bc229757fcdd9a26bdd080707b21a131f1a3d8
Renato <renato145@hotmail.com>
2021-04-18 23:01:04 +0930

    cra

---
+++ frontend/src/App.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const App = () => {
+  return <div className="h-screen w-screen bg-blue-200">asd</div>;
+};
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // mode: 'jit',
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      opacity: ["disabled"],
+      backgroundColor: ["active", "disabled"],
+    },
+  },
+  // plugins: [
+  //   require('@tailwindcss/forms'),
+  // ],
+};
